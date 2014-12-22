1c2eaccff7ad0c07487a26e19168e03467eeb11e
Patrick Johnson <im@pbj.me>
2014-12-22 16:09:45 -0500

    base layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+$(document).ready(function(){
+  $('.grid-list > li').on('click', function(){
+    var item = $(this).find('.grid-info');
+    if (item.hasClass('active')){
+      item.stop().fadeOut('400').removeClass('active');
+    } else {
+      $('.grid-info').stop().fadeOut('400').removeClass('active');
+      item.stop().fadeIn('400').addClass('active');
+    }
+  });
+});
